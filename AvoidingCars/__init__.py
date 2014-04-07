58d001c7aaebbb1de005eeed92e883628cb51acd
Khashayar Pourdeilami <kpourdeilami@gmail.com>
2014-04-06 20:58:18 -0400

    Added Traffic game

---
+++ AvoidingCars/__init__.py
@@ -0,0 +1,84 @@
+import pygame
+from pygame.locals import *
+from classes import *
+from menu import *
+import os
+
+class AvoidingCars():
+    """
+    Pre:
+    Post:
+    Purpose:
+    """
+    def __init__(self):
+        self.status = False
+    """
+    Pre:
+    Post:
+    Purpose:
+    """
+    def run(self):
+
+        width = 800
+        height = 600
+
+        pygame.init()
+        screen = pygame.display.set_mode((width, height))
+        pygame.display.set_caption('Avoiding Cars Game')
+        pygame.mouse.set_visible(1)
+        background = pygame.Surface(screen.get_size())
+        background = background.convert()
+        background.fill((0, 0, 0))
+        clock = pygame.time.Clock()
+
+
+
+        # draw background
+        screen.blit(background, (0, 0))
+        pygame.display.flip()
+
+
+        mainMenu = Menu(["Play", " ", "Quit"])
+        mainMenu.drawMenu()
+
+
+
+        while 1:
+            clock.tick(60)
+
+            for event in pygame.event.get():
+                mainMenu.handleEvent(event)
+                # quit the game if escape is pressed
+                if event.type == QUIT:
+                    return
+                elif event.type == KEYDOWN and event.key == K_ESCAPE:
+                    mainMenu.activate()
+                elif event.type == Menu.MENUCLICKEDEVENT:
+                    if event.text == "Quit":
+                        return
+                    elif event.item == 0:
+                        isGameActive = True
+                        mainMenu.deactivate()
+            screen.blit(background, (0, 0))
+            if mainMenu.isActive():
+                mainMenu.drawMenu()
+            else:
+                background.fill((229, 233, 236))
+
+
+            pygame.display.flip()
+    """
+    Pre:
+    Post:
+    Purpose:
+    """
+    def end(self):
+        self.status = False
+
+    """
+    Pre:
+    Post:
+    Purpose:
+    """
+    def restart(self):
+        pass
---
+++ AvoidingCars/classes.py
@@ -0,0 +1,18 @@
+import pygame
+from pygame.locals import *
+import ConfigParser
+import os
+scriptDir = os.path.dirname(__file__)
+
+class Player():
+    pass
+class City():
+    pass
+class Turn():
+    pass
+class Car():
+    pass
+class Obstacle():
+    pass
+
+
