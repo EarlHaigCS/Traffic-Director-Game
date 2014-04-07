0d0c75bcecda8ce24a21c13de83403ae2a109fea
Khashayar Pourdeilami <kpourdeilami@gmail.com>
2014-04-06 21:22:40 -0400

    Added the games

---
+++ ChooChooTrain/__init__.py
@@ -0,0 +1,7 @@
+import pygame
+from pygame.locals import *
+from classes import *
+from menu import *
+import os
+
+# main code
---
+++ ChooChooTrain/classes.py
@@ -0,0 +1,4 @@
+import pygame
+from pygame.locals import *
+
+#code for classes
